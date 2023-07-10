75487cfcfb27e588e8b36739e9b8158439d86aa7
Diana Langenshtras <300602did@mail.ru>
2023-07-10 18:45:00 +0300

    17

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
-{/*import Button from "../Button/Button"*/}
 import React from 'react';
 import { useTelegram } from '../../hooks/useTelegram';
 import './Header.css'
+//import Button from "../Button/Button"
 
 const Header = () => {
    {/* const {user, onClose} = useTelegram();*/}
